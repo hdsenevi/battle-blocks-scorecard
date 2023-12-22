b5c8c4ebfd468c4f3eb5a95b9c055b6bb6c1d631
hdsenevi <shanaka36@gmail.com>
2023-12-23 10:20:20 +1100

    Initial setup

---
+++ BattleBlocksScorecard/BattleBlocksScorecardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleBlocksScorecardApp.swift
+//  BattleBlocksScorecard
+//
+//  Created by Shanaka Senevirathne on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BattleBlocksScorecardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
